935d601ebddb9f8a480366184f3bb7bb253abf03
Jungillwoo <105035782+Jungillwoo@users.noreply.github.com>
2023-12-05 23:35:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ app.listen(8080, function(){
 });
 
 // 서버 끄기 컨트롤 c
-app.get('/pet', function(요청, 응답){
-    응답.send('펫쇼핑할 수 있는 사이트입니다.');
-});
+
 
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html')
